7c20a79f82e941f648ea13208e24892ba9670fff
K.T. Humaira <kt.humaira@gmail.com>
2026-06-16 22:52:46 +0600

    fix

--- talk-nook-home-main/src/routes/api/notify.ts
+++ talk-nook-home-main/src/routes/api/notify.ts
@@ -4,7 +4,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const APIRoute = createAPIFileRoute("/api/notify")({
+export const Route = createAPIFileRoute("/api/notify")({
   POST: async ({ request }) => {
     const { userName, service, txid, ref } = await request.json();
     
