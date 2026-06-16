b75c7422bbda76eb5c21359075720809cd76ca9b
K.T. Humaira <kt.humaira@gmail.com>
2026-06-16 16:34:08 +0600

    fix railway deployment

--- talk-nook-home-main/server/index.js
+++ talk-nook-home-main/server/index.js
@@ -33,4 +33,4 @@ app.post('/api/notify-booking', async (req, res) => {
     }
   });
 
-app.listen(4000, () => console.log('Server running on port 4000'));
+  app.listen(process.env.PORT || 4000, () => console.log('Server running on port', process.env.PORT || 4000));
